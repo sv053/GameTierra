ed4742f78f4cb71bcdf18522f60f6b154392be7f
sv053 <mirarlejos12@gmail.com>
2021-11-16 01:20:53 +0300

    with optional, and isolated unit tests

--- src/test/java/com/gamesage/store/service/GameServiceTest.java
+++ src/test/java/com/gamesage/store/service/GameServiceTest.java
@@ -18,37 +18,37 @@ class GameServiceTest {
 
     @Test
     void buyGame_True() {
-        final GameRepository repository = mock(GameRepository.class);
+        GameRepository repository = mock(GameRepository.class);
         final int gameId = 1;
 
-        final Game game = new Game(gameId, "lago", BigDecimal.ONE);
+        Game game = new Game(gameId, "lago", BigDecimal.ONE);
         when(repository.findBy(gameId)).thenReturn(Optional.of(game));
 
-        final GameService gameService = new GameService(repository);
+        GameService gameService = new GameService(repository);
 
-        final User user = new User(null, new Tier("", 18), BigDecimal.TEN);
+        User user = new User(null, new Tier("", 18), BigDecimal.TEN);
 
         assertTrue(gameService.buyGame(gameId, user));
     }
 
     @Test
-    void buyGame_RightNewBalance() {
-        final GameRepository repository = mock(GameRepository.class);
+    void buyGame_NewBalance() {
+        GameRepository repository = mock(GameRepository.class);
         final int gameId = 1;
 
-        final Game game = new Game(gameId, "mar", BigDecimal.ONE);
+        Game game = new Game(gameId, "mar", BigDecimal.ONE);
         when(repository.findBy(gameId)).thenReturn(Optional.of(game));
 
-        final GameService gameService = new GameService(repository);
+        GameService gameService = new GameService(repository);
 
-        final BigDecimal initBalance = BigDecimal.valueOf(156.82);
-        final User user = new User("", new Tier("", 18), initBalance);
+        BigDecimal initBalance = BigDecimal.valueOf(156.82);
+        User user = new User("", new Tier("", 18), initBalance);
 
         gameService.buyGame(gameId, user);
 
-        final BigDecimal cashback = game.getPrice().multiply(BigDecimal.valueOf
+        BigDecimal cashback = game.getPrice().multiply(BigDecimal.valueOf
                 (user.getTier().getCashbackPercentage()));
-        final BigDecimal expectedBalance = initBalance.subtract(game.getPrice()).add(cashback)
+        BigDecimal expectedBalance = initBalance.subtract(game.getPrice()).add(cashback)
                 .setScale(2, RoundingMode.HALF_UP);
 
         assertEquals(expectedBalance, user.getBalance());
@@ -56,16 +56,16 @@ class GameServiceTest {
 
     @Test
     void buyGame_PriceHigherThanBalance_SameBalance() {
-        final GameRepository repository = mock(GameRepository.class);
+        GameRepository repository = mock(GameRepository.class);
         final int gameId = 1;
 
-        final Game game = new Game(gameId, "addedGame", BigDecimal.TEN);
+        Game game = new Game(gameId, "addedGame", BigDecimal.TEN);
         when(repository.findBy(gameId)).thenReturn(Optional.of(game));
 
-        final GameService gameService = new GameService(repository);
+        GameService gameService = new GameService(repository);
 
-        final BigDecimal initBalance = BigDecimal.ONE;
-        final User user = new User("lark", new Tier("", 15), initBalance);
+        BigDecimal initBalance = BigDecimal.ONE;
+        User user = new User("lark", new Tier("", 15), initBalance);
 
         gameService.buyGame(gameId, user);
 
@@ -74,30 +74,30 @@ class GameServiceTest {
 
     @Test
     void buyGame_PriceHigherThanBalance_False() {
-        final GameRepository repository = mock(GameRepository.class);
+        GameRepository repository = mock(GameRepository.class);
         final int gameId = 1;
 
-        final Game game = new Game(gameId, "rio", BigDecimal.ONE);
+        Game game = new Game(gameId, "rio", BigDecimal.ONE);
         when(repository.findBy(gameId)).thenReturn(Optional.of(game));
 
-        final GameService gameService = new GameService(repository);
+        GameService gameService = new GameService(repository);
 
-        final User user = new User("", new Tier(null, .0), BigDecimal.ZERO);
+        User user = new User("", new Tier(null, .0), BigDecimal.ZERO);
 
         assertFalse(gameService.buyGame(game.getId(), user));
     }
 
     @Test
     void buyGame_AlreadyOwned_True() {
-        final GameRepository repository = mock(GameRepository.class);
+        GameRepository repository = mock(GameRepository.class);
         final int gameId = 1;
 
-        final Game game = new Game(gameId, "isla", BigDecimal.ONE);
+        Game game = new Game(gameId, "isla", BigDecimal.ONE);
         when(repository.findBy(gameId)).thenReturn(Optional.of(game));
 
-        final GameService gameService = new GameService(repository);
+        GameService gameService = new GameService(repository);
 
-        final User user = new User(null, null, BigDecimal.TEN);
+        User user = new User(null, null, BigDecimal.TEN);
         user.addGame(game);
 
         gameService.buyGame(game.getId(), user);
@@ -107,37 +107,37 @@ class GameServiceTest {
 
     @Test
     void findById_FoundGame() {
-        final GameRepository repository = mock(GameRepository.class);
+        GameRepository repository = mock(GameRepository.class);
         final int gameId = 1;
-        final Game game = new Game(gameId, "fabula", BigDecimal.ONE);
+        Game game = new Game(gameId, "fabula", BigDecimal.ONE);
         when(repository.findBy(gameId)).thenReturn(Optional.of(game));
 
-        final GameService gameService = new GameService(repository);
+        GameService gameService = new GameService(repository);
 
         assertEquals(game, gameService.findById(1));
     }
 
     @Test
     void findById_GameDoesntExist_IAE() {
-        final GameRepository repository = mock(GameRepository.class);
+        GameRepository repository = mock(GameRepository.class);
         final int gameId = 1;
         when(repository.findBy(gameId)).thenReturn(Optional.empty());
-        final GameService gameService = new GameService(repository);
+        GameService gameService = new GameService(repository);
 
         assertThrows(IllegalArgumentException.class, () -> gameService.findById(gameId));
     }
 
     @Test
     void calculateCashback_CorrectCashback() {
-        final GameRepository repository = mock(GameRepository.class);
+        GameRepository repository = mock(GameRepository.class);
         final int gameId = 1;
-        final Game game = new Game(gameId, "fabula", BigDecimal.ONE);
-        final GameService gameService = new GameService(repository);
+        Game game = new Game(gameId, "fabula", BigDecimal.ONE);
+        GameService gameService = new GameService(repository);
 
-        final User user = new User("marvel", new Tier("", 5.), BigDecimal.TEN);
+        User user = new User("marvel", new Tier("", 5.), BigDecimal.TEN);
 
-        final BigDecimal cashbackPercentage = BigDecimal.valueOf(user.getTier().getCashbackPercentage());
-        final BigDecimal expectedCashback = game.getPrice().multiply(cashbackPercentage);
+        BigDecimal cashbackPercentage = BigDecimal.valueOf(user.getTier().getCashbackPercentage());
+        BigDecimal expectedCashback = game.getPrice().multiply(cashbackPercentage);
 
         assertEquals(expectedCashback, gameService.calculateCashback(game.getPrice(), user));
     }
