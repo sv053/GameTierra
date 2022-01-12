ffd35dd73cd063228fc32c668fa5e9f21a90c800
sv053 <mirarlejos12@gmail.com>
2022-01-12 20:09:22 +0500

    database with settings are added

--- src/main/java/com/gamesage/store/GameTierra.java
+++ src/main/java/com/gamesage/store/GameTierra.java
@@ -1,13 +1,35 @@
 package com.gamesage.store;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 @SpringBootApplication
 public class GameTierra {
 
+    private static final Logger logger = LoggerFactory.getLogger(GameTierra.class);
+
     public static void main(String[] args) {
         SpringApplication.run(GameTierra.class);
+
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:aceofgames.db");
+            if (connection != null) {
+                logger.info("Database is connected");
+            } else {
+                for (StackTraceElement el : Thread.currentThread().getStackTrace()) {
+                    logger.error(el.getMethodName());
+                }
+            }
+        } catch (Exception ex) {
+            logger.error(ex.getMessage());
+        }
     }
 }
 
