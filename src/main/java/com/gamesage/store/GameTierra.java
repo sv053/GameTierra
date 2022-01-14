19e7fceff140eb909a21fed179cfd18fe0e9ca1a
sv053 <mirarlejos12@gmail.com>
2022-01-15 00:22:33 +0500

    REST API URL pattern and DB schema

--- src/main/java/com/gamesage/store/GameTierra.java
+++ src/main/java/com/gamesage/store/GameTierra.java
@@ -4,9 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GameTierra {
@@ -14,15 +15,23 @@ public class GameTierra {
     private static final Logger logger = LoggerFactory.getLogger(GameTierra.class);
 
     public static void main(String[] args) {
+
         SpringApplication.run(GameTierra.class);
 
-        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:aceofgames.db")) {
-            if (connection != null) {
-                logger.info("Database is connected");
-            }
-        } catch (Exception ex) {
-            logger.error(ex.getMessage());
-        }
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.setPort(8888);
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory>
+    webServerFactoryCustomizer() {
+
+        String rootContext = new StringBuilder()
+                .append("/")
+                .append(GameTierra.logger.getName().split("\\.")[1])
+                .append("/api")
+                .toString();
+        return factory -> factory.setContextPath(rootContext);
     }
 }
 
