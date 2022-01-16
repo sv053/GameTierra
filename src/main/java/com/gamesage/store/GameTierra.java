71ad4b9c1a8d6cacff9f7b58aa55cdf80e00dc2a
sv053 <mirarlejos12@gmail.com>
2022-01-16 07:24:43 +0500

    A REST API URL as a string constant and a readable ddl script

--- src/main/java/com/gamesage/store/GameTierra.java
+++ src/main/java/com/gamesage/store/GameTierra.java
@@ -1,37 +1,16 @@
 package com.gamesage.store;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
+
+import static java.lang.System.setProperty;
 
 @SpringBootApplication
 public class GameTierra {
 
-    private static final Logger logger = LoggerFactory.getLogger(GameTierra.class);
-
     public static void main(String[] args) {
-
+        setProperty("server.servlet.context-path", "/gamesage/api");
         SpringApplication.run(GameTierra.class);
-
-        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setPort(8888);
-    }
-
-    @Bean
-    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory>
-    webServerFactoryCustomizer() {
-
-        String rootContext = new StringBuilder()
-                .append("/")
-                .append(GameTierra.logger.getName().split("\\.")[1])
-                .append("/api")
-                .toString();
-        return factory -> factory.setContextPath(rootContext);
     }
 }
 
