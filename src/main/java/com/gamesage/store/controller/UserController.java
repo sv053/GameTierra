67d62e017ecd991a924b0efa190f15ef3e7367dc
sv053 <mirarlejos12@gmail.com>
2022-03-04 23:21:54 +0500

    balance top up method

--- src/main/java/com/gamesage/store/controller/UserController.java
+++ src/main/java/com/gamesage/store/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.gamesage.store.controller;
 
+import com.gamesage.store.domain.model.Card;
 import com.gamesage.store.domain.model.User;
 import com.gamesage.store.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -29,6 +32,21 @@ public class UserController {
     @PostMapping
     public User createOne(@RequestBody User user) {
         return userService.createOne(user);
+    }
+
+    @PostMapping("/{id}/{cardNumber}/{cardholder}/{date}/{cvc}/{amount}")
+    public User topUp(@RequestBody Integer id, @RequestBody Long cardNumber,
+                      @RequestBody LocalDateTime date, @RequestBody String cardholder,
+                      @RequestBody Integer cvc, @RequestBody BigDecimal amount) {
+        boolean paid = false;
+        User user = null;
+        Card card = new Card(cardNumber, cardholder, date, cvc, amount);
+        // third party api checks the card, helps to pay and returns bool
+        // paid = true/false;
+        if (paid) {
+            user = userService.updateBalance(userService.findById(id));
+        }
+        return user;
     }
 }
 
---
+++ src/main/java/com/gamesage/store/domain/model/Card.java
@@ -0,0 +1,24 @@
+package com.gamesage.store.domain.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Card {
+
+    private final Long cardNumber16;
+    private final String cardholderName;
+    private final LocalDateTime expireDate;
+    private final Integer cvcCode;
+    private final BigDecimal amountToPay;
+
+
+    public Card(Long cardNumber16, String cardholderName, LocalDateTime expireDate,
+                Integer cvcCode, BigDecimal amountToPay) {
+        this.cardNumber16 = cardNumber16;
+        this.cardholderName = cardholderName;
+        this.expireDate = expireDate;
+        this.cvcCode = cvcCode;
+        this.amountToPay = amountToPay;
+    }
+}
+
