e8f802470783745ec05d8f1db1a9e2d50a22e898
sv053 <mirarlejos12@gmail.com>
2022-06-13 01:28:53 +0500

    configure fixed

--- src/main/java/com/gamesage/store/configs/WebSecurityConfig.java
+++ src/main/java/com/gamesage/store/configs/WebSecurityConfig.java
@@ -1,19 +1,30 @@
 package com.gamesage.store.configs;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-
+@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
+                .httpBasic()
+                .and()
                 .authorizeRequests()
-                .antMatchers("/", "*/games").permitAll()
-                .antMatchers("*/login", "*/login/*").authenticated()
+                .antMatchers("/games", "/games/**")
+                .permitAll()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/login", "/login/**")
+                .hasRole("ADMIN")
+                .anyRequest()
+                .authenticated()
                 .and()
                 .logout()
                 .permitAll();
     }
 }
+
