dae2498a6f1246a951900888c63018287721aa89
sv053 <mirarlejos12@gmail.com>
2022-06-15 02:12:07 +0500

    cleaned

--- src/main/java/com/gamesage/store/configs/WebSecurityConfig.java
+++ src/main/java/com/gamesage/store/configs/WebSecurityConfig.java
@@ -1,24 +1,11 @@
 package com.gamesage.store.configs;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    public WebSecurityConfig() {
-
-        //   securityContext = SecurityContextHolder.getContext();
-        //   this.user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-    }
-
-//    public SecurityProperties.User getUser() {
-//        return user;
-//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -37,16 +24,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                 .permitAll();
     }
-
 }
-
-//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//                .inMemoryAuthentication()
-//                .withUser("user")
-//                .password(passwordEncoder.encode("password"))
-//                .roles("USER");
-//
-//    }
-//}
 
