5fa8a5dd1efe0f3299c14e75c20eecb147cf1fec
sv053 <mirarlejos12@gmail.com>
2022-06-15 17:36:27 +0500

    AuthService as a service

--- src/main/java/com/gamesage/store/controller/LoginController.java
+++ src/main/java/com/gamesage/store/controller/LoginController.java
@@ -21,7 +21,7 @@ public class LoginController {
 
     @PostMapping
     public ResponseEntity<User> login(@RequestBody User user) {
-        if (authService.findByLoginPassword(user.getName(), user.getPassword()))
+        if (authService.ifCredentialsExist(user.getName(), user.getPassword()))
             return new ResponseEntity<>(user, HttpStatus.OK);
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
--- src/main/java/com/gamesage/store/service/AuthService.java
+++ src/main/java/com/gamesage/store/service/AuthService.java
@@ -1,34 +1,17 @@
 package com.gamesage.store.service;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
-@ConfigurationProperties(prefix = "spring.security.user")
-@Configuration
 public class AuthService {
 
+    @Value("${spring.security.user.name}")
     private String name;
+    @Value("${spring.security.user.password}")
     private String password;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public boolean findByLoginPassword(String login, String pass) {
+    public boolean ifCredentialsExist(String login, String pass) {
         return login.equals(name) && password.equals(pass);
     }
 }
