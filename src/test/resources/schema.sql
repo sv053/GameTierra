52f00ace563b6b8a01a9c8fd086b78f27726b855
sv053 <mirarlejos12@gmail.com>
2022-06-16 06:06:05 +0500

    sql schema format fixed

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,44 +1,30 @@
 CREATE TABLE IF NOT EXISTS game
 (
-    id    INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id    INTEGER PRIMARY KEY AUTOINCREMENT,
     name  VARCHAR(350) NOT NULL UNIQUE,
     price REAL
 );
 
 CREATE TABLE IF NOT EXISTS tier
 (
-    id         INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id         INTEGER PRIMARY KEY AUTOINCREMENT,
     name       VARCHAR(30) NOT NULL UNIQUE,
     percentage REAL
 );
 
 CREATE TABLE IF NOT EXISTS user
 (
-    id
-    INTEGER
-    PRIMARY
-    KEY
-    AUTO_INCREMENT,
-    login
-    VARCHAR
-(
-    150
-) NOT NULL UNIQUE,
-    balance REAL,
-    tier_id INTEGER,
-    password TEXT,
-    FOREIGN KEY
-(
-    tier_id
-) REFERENCES tier
-(
-    id
-)
-    );
+    id       INTEGER PRIMARY KEY AUTOINCREMENT,
+    login    VARCHAR(150) NOT NULL UNIQUE,
+    balance  REAL,
+    tier_id  INTEGER,
+    password VARCHAR(150),
+    FOREIGN KEY (tier_id) REFERENCES tier (id)
+);
 
 CREATE TABLE IF NOT EXISTS orders
 (
-    id             INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id             INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id        INTEGER,
     game_id        INTEGER,
     order_datetime DATETIME,
