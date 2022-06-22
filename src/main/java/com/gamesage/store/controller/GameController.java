9e7f563538ffe11a8ea8a4aa92c0e6254bd62ba8
sv053 <mirarlejos12@gmail.com>
2022-06-22 19:50:28 +0500

    REST restored

--- src/main/java/com/gamesage/store/controller/GameController.java
+++ src/main/java/com/gamesage/store/controller/GameController.java
@@ -26,7 +26,7 @@ public class GameController {
         return gameService.findAll();
     }
 
-    @PostMapping("/add")
+    @PostMapping
     public List<Game> createGames(@RequestBody List<Game> gamesToSave) {
         return gameService.createAll(gamesToSave);
     }
