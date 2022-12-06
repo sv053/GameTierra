953827567d0f13a4a4e86a44f863f43b92f96dbc
sv053 <mirarlejos12@gmail.com>
2022-12-06 22:34:57 +0500

    password encoder renamed

--- src/main/java/com/gamesage/store/security/config/PasswordEncoderImpl.java
+++ src/main/java/com/gamesage/store/security/config/PasswordEncoderImpl.java
@@ -1,11 +1,11 @@
-package com.gamesage.store.security.encoder;
+package com.gamesage.store.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class PasswordEncoderConfig {
+public class PasswordEncoderImpl {
 
     @Bean
     public BCryptPasswordEncoder encoder() {
