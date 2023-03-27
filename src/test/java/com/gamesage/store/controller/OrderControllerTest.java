6e93100ebe5b293be890e8fe4f8dee5ccceda5d0
sv053 <mirarlejos12@gmail.com>
2023-03-28 03:16:02 +0500

    19-ordersController is out of order

--- src/test/java/com/gamesage/store/controller/OrderControllerTest.java
+++ src/test/java/com/gamesage/store/controller/OrderControllerTest.java
@@ -1,13 +1,12 @@
 package com.gamesage.store.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.gamesage.store.domain.model.AuthToken;
 import com.gamesage.store.domain.model.Game;
 import com.gamesage.store.domain.model.Tier;
 import com.gamesage.store.domain.model.User;
 import com.gamesage.store.service.GameService;
-import com.gamesage.store.service.TokenService;
 import com.gamesage.store.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -18,7 +17,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
-import java.util.Optional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -39,52 +37,82 @@ class OrderControllerTest {
     @Autowired
     private UserService userService;
     @Autowired
-    private TokenService tokenService;
-    @Autowired
     private GameService gameService;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private String userJson;
+
+    @BeforeEach
+    void setup() {
+        userJson = "{\"id\": 2, " +
+                " \"login\": \"admin\", " +
+                " \"password\": \"letmein\", " +
+                " \"tier\": { " +
+                " \"cashbackPercentage\": 0.0, " +
+                " \"id\": 1, " +
+                " \"level\": \"FREE\" " +
+                "}, " +
+                "\"balance\": 815.16, " +
+                "\"games\": []}";
+    }
 
     @Test
     void givenRightCreds_shouldFindOrderById() throws Exception {
+        Game game = new Game(2, "THE_LAST_OF_US", BigDecimal.valueOf(7.28d));
+        Game savedGame = gameService.createOne(game);
+
         User userWithRightCreds = new User(111, "admin", "letmein", new Tier(
                 1, "FREE", 0.d), BigDecimal.TEN);
-
         User savedUser = userService.createOne(userWithRightCreds);
 
-        mockMvc.perform(post(API_LOGIN_ENDPOINT)
-                        .content(new ObjectMapper().writeValueAsString(userWithRightCreds))
+        String tokenValue = mockMvc.perform(post(API_LOGIN_ENDPOINT)
+                        .content(userJson)
                         .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getHeader("X-Auth-Token");
+
+        mockMvc.perform(post(API_ORDER_ENDPOINT + "/{gameId}/{userId}", savedGame.getId(), savedUser.getId())
+                        .header("X-Auth-Token", tokenValue)
+                        // .content(userJson)
+                        .contentType(APPLICATION_JSON_UTF8))
                 .andExpect(status().isOk());
-
-        Optional<AuthToken> token = tokenService.findTokenByLogin(savedUser.getLogin());
-
-        String tokenValue = "";
-        if (token.isPresent()) {
-            tokenValue = token.get().getValue();
-        }
 
         mockMvc.perform(get(API_ORDER_ENDPOINT + "/1")
                         .header("X-Auth-Token", tokenValue)
                         .contentType(APPLICATION_JSON_UTF8))
                 .andExpect(status().isOk());
+        //  .andExpect(jsonPath("$[0].userId").value(savedUser.getId()))
+        // .andExpect(jsonPath("$[0].gameId").value(savedGame.getId()));
     }
 
     @Test
     void givenWrongCreds_shouldNotFindOrderById() throws Exception {
-        User userWithRightCreds = new User(111, "admin", "letmein", new Tier(
+        User user = new User(111, "admin", "letmein", new Tier(
                 1, "FREE", 0.d), BigDecimal.TEN);
-
-        User savedUser = userService.createOne(userWithRightCreds);
-
-        mockMvc.perform(post(API_LOGIN_ENDPOINT)
-                        .content(new ObjectMapper().writeValueAsString(userWithRightCreds))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-
+        User savedUser = userService.createOne(user);
         Game game = new Game(2, "THE_LAST_OF_US", BigDecimal.valueOf(7.28d));
         Game savedGame = gameService.createOne(game);
+        int orderId = 77777;
 
-        mockMvc.perform(get(API_ORDER_ENDPOINT + "/{gameId}/{userId}", savedGame.getId(), savedUser.getId())
-                        .content(new ObjectMapper().writeValueAsString(savedUser))
+        mockMvc.perform(post("/games")
+                        .content(objectMapper.writeValueAsString(game))
+                        .contentType(APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk());
+
+        String tokenValue = mockMvc.perform(post(API_LOGIN_ENDPOINT)
+                        .content(userJson)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getHeader("X-Auth-Token");
+
+        mockMvc.perform(post(API_ORDER_ENDPOINT + "/{gameId}/{userId}", 2, 2)
+                        .header("X-Auth-Token", tokenValue)
+                        .content(userJson)
                         .contentType(APPLICATION_JSON_UTF8))
                 .andExpect(status().isOk());
 
@@ -97,27 +125,21 @@ class OrderControllerTest {
     void givenRightCreds_shouldFindAllOrders() throws Exception {
         User userWithRightCreds = new User(111, "admin", "letmein", new Tier(
                 1, "FREE", 0.d), BigDecimal.TEN);
-
         User savedUser = userService.createOne(userWithRightCreds);
-
-        mockMvc.perform(post(API_LOGIN_ENDPOINT)
-                        .content(new ObjectMapper().writeValueAsString(userWithRightCreds))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-
-        Game game = new Game(2, "THE_LAST_OF_US", BigDecimal.valueOf(7.28d));
+        Game game = new Game(1, "THE_LAST_OF_US", BigDecimal.valueOf(7.28d));
         Game savedGame = gameService.createOne(game);
 
-        Optional<AuthToken> token = tokenService.findTokenByLogin(savedUser.getLogin());
+        String tokenValue = mockMvc.perform(post(API_LOGIN_ENDPOINT)
+                        .content(userJson)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getHeader("X-Auth-Token");
 
-        String tokenValue = "";
-        if (token.isPresent()) {
-            tokenValue = token.get().getValue();
-        }
-
-        mockMvc.perform(get(API_ORDER_ENDPOINT + "/{gameId}/{userId}", savedGame.getId(), savedUser.getId())
+        mockMvc.perform(post(API_ORDER_ENDPOINT + "/{gameId}/{userId}", savedGame.getId(), 2)
                         .header("X-Auth-Token", tokenValue)
-                        .content(new ObjectMapper().writeValueAsString(savedUser))
+                        .content(userJson)
                         .contentType(APPLICATION_JSON_UTF8))
                 .andExpect(status().isOk());
 
@@ -131,26 +153,21 @@ class OrderControllerTest {
     void givenRightCreds_shouldBuyGame() throws Exception {
         User userWithRightCreds = new User(111, "admin", "letmein", new Tier(
                 1, "FREE", 0.d), BigDecimal.TEN);
-
         User savedUser = userService.createOne(userWithRightCreds);
-
-        mockMvc.perform(post(API_LOGIN_ENDPOINT)
-                        .content(new ObjectMapper().writeValueAsString(userWithRightCreds))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-
-        Optional<AuthToken> token = tokenService.findTokenByLogin(userWithRightCreds.getLogin());
-
-        String tokenValue = "";
-        if (token.isPresent()) {
-            tokenValue = token.get().getValue();
-        }
         Game game = new Game(2, "THE_LAST_OF_US", BigDecimal.valueOf(7.28d));
         Game savedGame = gameService.createOne(game);
 
-        mockMvc.perform(get(API_ORDER_ENDPOINT + "/{gameId}/{userId}", savedGame.getId(), savedUser.getId())
+        String tokenValue = mockMvc.perform(post(API_LOGIN_ENDPOINT)
+                        .content(userJson)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getHeader("X-Auth-Token");
+
+        mockMvc.perform(post(API_ORDER_ENDPOINT + "/{gameId}/{userId}", savedGame.getId(), savedUser.getId())
                         .header("X-Auth-Token", tokenValue)
-                        .content(new ObjectMapper().writeValueAsString(savedUser))
+                        .content(userJson)
                         .contentType(APPLICATION_JSON_UTF8))
                 .andExpect(status().isOk());
     }
@@ -167,11 +184,10 @@ class OrderControllerTest {
                 .andExpect(status().isUnauthorized());
 
         Game game = new Game(2, "THE_LAST_OF_US", BigDecimal.valueOf(7.28d));
-
         Game savedGame = gameService.createOne(game);
 
-        mockMvc.perform(get(API_ORDER_ENDPOINT + "/{gameId}/{userId}", savedGame.getId(), savedUser.getId())
-                        .content(new ObjectMapper().writeValueAsString(userWithRightCreds))
+        mockMvc.perform(post(API_ORDER_ENDPOINT + "/{gameId}/{userId}", savedGame.getId(), savedUser.getId())
+                        .content(userJson)
                         .contentType(APPLICATION_JSON_UTF8))
                 .andExpect(status().isUnauthorized());
     }
