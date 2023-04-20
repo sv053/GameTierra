ac2ad74749f6bbe584a39c6199f5fec6c47269eb
sv053 <mirarlejos12@gmail.com>
2023-04-20 22:22:06 +0500

    19-tests ref 7.0

--- src/main/java/com/gamesage/store/exception/AlreadyTakenLoginException.java
+++ src/main/java/com/gamesage/store/exception/AlreadyTakenLoginException.java
@@ -3,7 +3,7 @@ package com.gamesage.store.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE, reason = "login is taken")
+@ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "login is taken")
 public class AlreadyTakenLoginException extends RuntimeException {
 
     public AlreadyTakenLoginException() {
