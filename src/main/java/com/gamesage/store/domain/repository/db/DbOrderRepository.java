7b7f1b9490858804383f7469c8459edfacfe9d56
sv053 <mirarlejos12@gmail.com>
2023-05-15 04:39:02 +0500

    20 - order query

--- src/main/java/com/gamesage/store/domain/repository/db/DbOrderRepository.java
+++ src/main/java/com/gamesage/store/domain/repository/db/DbOrderRepository.java
@@ -22,25 +22,21 @@ public class DbOrderRepository implements Repository<Order, Integer> {
 
     private static final String INSERT_ORDER = "INSERT INTO orders (user_id, game_id, order_datetime) " +
             "VALUES (?, ?, ?) ";
-    private static final String SELECT_ALL_ORDERS_QUERY =
+    private static final String SELECT_ORDERS_QUERY =
             "SELECT orders.id AS id, user_id, game_id, order_datetime, " +
                     " user.login, user.password, user.tier_id, user.balance, " +
                     " game.id, game.name, game.price" +
                     " FROM orders " +
-                    " LEFT JOIN user ON  user.id = user_id " +
-                    " LEFT JOIN game ON game.id = game_id " +
-                    " WHERE user.login IS NOT NULL " +
-                    " AND game.name IS NOT NULL ";
-    private static final String SELECT_ALL_ORDERS_SYNC_EXISTENT_USERS_QUERY =
+                    " INNER JOIN user ON  user.id = user_id " +
+                    " INNER JOIN game ON game.id = game_id ";
+    private static final String SELECT_ORDER_QUERY =
             "SELECT orders.id AS id, user_id, game_id, order_datetime, " +
                     " user.login, user.password, user.tier_id, user.balance, " +
-                    " game.id, game.name, game.price" +
-                    " FROM user " +
-                    " LEFT JOIN orders ON  user.id = user_id " +
+                    " game.id, game.name, game.price " +
+                    " FROM orders " +
+                    " LEFT JOIN user ON  user.id = user_id " +
                     " LEFT JOIN game ON game.id = game_id " +
-                    " WHERE orders.id IS NOT NULL " +
-                    " AND game.name IS NOT NULL ";
-    private static final String SELECT_ORDER_QUERY = SELECT_ALL_ORDERS_QUERY + " AND orders.id = ?";
+                    " WHERE orders.id = ?";
     private final JdbcTemplate jdbcTemplate;
     private final RowMapper<Order> rowMapper;
 
@@ -81,7 +77,7 @@ public class DbOrderRepository implements Repository<Order, Integer> {
 
     @Override
     public List<Order> findAll() {
-        return jdbcTemplate.query(SELECT_ALL_ORDERS_SYNC_EXISTENT_USERS_QUERY, rowMapper);
+        return jdbcTemplate.query(SELECT_ORDERS_QUERY, rowMapper);
     }
 
     @Component
